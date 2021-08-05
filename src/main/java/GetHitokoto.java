edbe22d632cf21d42b0daa5536105ef833e1c9b2
LDS_XiaoYe <xiaoyecreate@outlook.com>
2021-08-05 16:05:43 +0800

    Upload files

---
+++ src/main/java/GetHitokoto.java
@@ -0,0 +1,25 @@
+import com.alibaba.fastjson.JSONObject;
+import utils.OkHttpApi;
+
+import java.io.IOException;
+
+public class GetHitokoto {
+
+    public String getHitokoto() throws IOException {
+        OkHttpApi api = new OkHttpApi();
+
+        String run = api.run("https://v1.hitokoto.cn");
+        var jsonObject = JSONObject.parseObject(run);
+        String content = jsonObject.getString("hitokoto");
+
+        return content;
+    }
+
+    public static void main(String[] args) throws IOException {
+        var object = new GetHitokoto();
+
+        for(int i = 0;i < 3;i++){
+            System.out.println(object.getHitokoto());;
+        }
+    }
+}
